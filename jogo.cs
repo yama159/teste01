1f445264ccd42850f37499cd20f3c90ee8b8bd8c
yama159 <fehiya@gmail.com>
2017-05-16 20:44:00 -0300

    jogo de adivinhação

---
+++ jogo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula2_ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            int numero = rnd.Next(0, 10);
+            bool acerto = false;
+            int tentativas = 1;
+            Console.WriteLine("Adivinhe o número entre 1 à 10.");
+            do
+            {
+
+                Console.Write("digite o numero: ");
+                int num = Convert.ToInt16(Console.ReadLine());
+
+                if (num == numero)
+                {
+                    acerto = true;
+                    Console.WriteLine("Você acertou em " + tentativas + " tentativas");
+
+                }
+                else
+                {
+                    Console.Write("Você errou, ");
+                    if (num > numero)
+                    {
+                        Console.WriteLine("o número é menor ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("o número é maior ");
+                    }
+
+                    tentativas++;
+                }
+
+
+            } while (acerto == false);
+
+
+
+
+
+
+
+        }
+    }
+}
